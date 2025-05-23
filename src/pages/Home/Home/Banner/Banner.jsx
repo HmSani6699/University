1b5feb99162127094592acb7aa2ad7e191d1c1c6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-05-23 12:29:59 +0600

    aded banner

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -10,7 +10,6 @@ import { EffectFade, Navigation, Pagination, Autoplay } from "swiper/modules";
 import school from "../../../../../public/school.jpg";
 import schoo2 from "../../../../../public/school2.jpg";
 import schoo3 from "../../../../../public/school3.jpg";
-import schoo4 from "../../../../../public/school4.jpg";
 
 const Banner = () => {
   return (
@@ -25,33 +24,60 @@ const Banner = () => {
           disableOnInteraction: false,
         }}
         modules={[EffectFade, Navigation, Pagination, Autoplay]}
-        className="mySwiper"
+        className="mySwiper custom-swiper-nav my-swiper"
       >
         <SwiperSlide>
-          <div className="h-[500px] w-full">
+          <div className="relative h-[500px] w-full overflow-hidden">
+            {/* Blurred Background Image */}
             <img
               src={school}
               alt="school"
               className="h-full w-full object-cover"
             />
+
+            {/* Overlay Content */}
+            <div className="absolute inset-0 bg-[#00000085] bg-opacity-30 flex items-center justify-center text-center text-white">
+              <div>
+                <h1 className="text-white text-[40px] font-bold">
+                  সকলকে স্বাগতম,
+                </h1>
+                <p>সপ্তবর্ণ মডেল স্কুলকে সারা বাংলাদেশের রোল মডেল করতে চাই</p>
+              </div>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[500px] w-full">
+          <div className="relative h-[500px] w-full overflow-hidden">
+            {/* Blurred Background Image */}
             <img
               src={schoo2}
               alt="school"
               className="h-full w-full object-cover"
             />
+
+            {/* Overlay Content */}
+            <div className="absolute inset-0 bg-[#00000085] bg-opacity-30 flex items-center justify-center">
+              <h1 className="text-white text-3xl font-bold">
+                Welcome to Our School
+              </h1>
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="h-[500px] w-full">
+          <div className="relative h-[500px] w-full overflow-hidden">
+            {/* Blurred Background Image */}
             <img
               src={schoo3}
               alt="school"
               className="h-full w-full object-cover"
             />
+
+            {/* Overlay Content */}
+            <div className="absolute inset-0 bg-[#00000085] bg-opacity-30 flex items-center justify-center">
+              <h1 className="text-white text-3xl font-bold">
+                Welcome to Our School
+              </h1>
+            </div>
           </div>
         </SwiperSlide>
       </Swiper>
